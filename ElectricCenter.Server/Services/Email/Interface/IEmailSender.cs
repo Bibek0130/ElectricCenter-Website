7057203d595874d6bdf2aac7e0ead1a101f59b0e
Bibek Mainali <mainalibibek01@gmail.com>
2026-03-31 00:17:28 +0545

    Dev: Created Email Sender services.

---
+++ ElectricCenter.Server/Services/Email/Interface/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace ElectricCenter.Server.Services.Email.Interface
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string emial, string subject, string message);
+    }
+}
---
+++ ElectricCenter.Server/Services/Email/Service/EmailSender.cs
@@ -0,0 +1,29 @@
+﻿using ElectricCenter.Server.Services.Email.Interface;
+using System.Net;
+using System.Net.Mail;
+
+namespace ElectricCenter.Server.Services.Email.Service
+{
+    public class EmailSender: IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mail = "aQ9zXp7r@mail.com";
+            var pw = "!9$dY2*gH7kL";
+
+            var client = new SmtpClient("smtp.mail.com", 587)
+            {
+                Credentials = new NetworkCredential(mail, pw),
+                EnableSsl = true
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(
+                    from: mail,
+                    to: email,
+                    subject: subject,
+                    message
+                    ));
+        }
+    }
+}
