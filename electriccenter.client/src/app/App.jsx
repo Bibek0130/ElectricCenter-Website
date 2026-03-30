ffc11ca56a5a989acce8dbf22459da0554b0ea87
Bibek Mainali <mainalibibek01@gmail.com>
2026-03-30 19:47:30 +0545

    Dev: Fixed routing problems.

--- electriccenter.client/src/app/App.jsx
+++ electriccenter.client/src/app/App.jsx
@@ -1,12 +1,25 @@
 //imports
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import CustomNav from './layout/MainLayout.jsx';
 
 function App() {
     return(
         <>
             <Routes>
-           <Route index element={<CustomNav />} />
+                <Route element={<CustomNav li={[
+                    ["Home", "home"],
+                    ["About Us", "about"],
+                    ["Services", "services"],
+                    ["Common Electrical Problem", "FAQ"]
+                ]} />}>
+                    <Route path="/" element={<Navigate to="/home" replace />} />
+
+                    {/* 2. Your existing routes */}
+                    <Route path="/home" element={<h1>Home Page</h1>} />
+                    <Route path="/about" element={<h1>About Us</h1>} />
+                    <Route path="/services" element={<h1>Our Services</h1>} />
+                    <Route path="/faq" element={<h1>Electrical FAQ</h1>} />
+                </Route>
            </Routes>
         </>
     );
