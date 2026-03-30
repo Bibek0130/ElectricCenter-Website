ab5ab78d70c0935ddf2e0ed97886880b4b290501
Bibek Mainali <mainalibibek01@gmail.com>
2026-03-31 00:18:39 +0545

    Dev: Added new controller method for Sending emial.

--- ElectricCenter.Server/Controllers/WeatherForecastController.cs
+++ ElectricCenter.Server/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using ElectricCenter.Server.Services.Email.Interface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ElectricCenter.Server.Controllers
@@ -6,11 +7,31 @@ namespace ElectricCenter.Server.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+
+        private readonly IEmailSender _emailSender;
         private static readonly string[] Summaries =
         [
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         ];
 
+        public WeatherForecastController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        //method for testing email sending
+        public async Task<IActionResult> SendTestEmail() 
+        {
+            var receiver = "sua03@scolore.com";
+            var subject = "Test Email";
+            var message = "This is a test email sent from the WeatherForecastController.";
+
+            await _emailSender.SendEmailAsync(receiver, subject, message);
+
+            return Ok("Test email sent successfully.");
+
+
+        }
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
