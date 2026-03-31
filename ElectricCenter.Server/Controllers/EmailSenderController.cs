894235afb3b68e525fddb769cd0a4bb1861bcf43
Bibek Mainali <mainalibibek01@gmail.com>
2026-04-01 00:24:53 +0545

    Dev: Created EmailSender Controller.

---
+++ ElectricCenter.Server/Controllers/EmailSenderController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ElectricCenter.Server.Services.Email.Interface;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace ElectricCenter.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailSenderController: ControllerBase
+    {
+        private readonly IEmailSender _emailSender;
+
+        public EmailSenderController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        //method for testing email sending
+        [HttpPost]
+        public async Task<IActionResult> SendTestEmail()
+        {
+            var receiver = "buzcx58471@minitts.net";
+            var subject = "Test Email";
+            var message = "This is a test email sent from the WeatherForecastController.";
+
+            await _emailSender.SendEmailAsync(receiver, subject, message);
+
+            return Ok("Email send sucessfully");
+        }
+    }
+}
