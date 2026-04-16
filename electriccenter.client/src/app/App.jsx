90c0df080ec51a4b90a0633aa7438e5ee7d5f4e0
Bibek Mainali <mainalibibek01@gmail.com>
2026-04-16 18:56:30 +0545

    Fix: Fixed routing.

--- electriccenter.client/src/app/App.jsx
+++ electriccenter.client/src/app/App.jsx
@@ -5,6 +5,7 @@ import LandingPage from '../features/landing page/LandingPage.jsx';
 import ProductsPage from '../features/Product/pages/ProductsPage.jsx';
 import ServicePage from '../features/service/pages/service.jsx';
 import FAQPage from '../features/FAQ/FAQ page.jsx';
+import AboutPage from '../../src/features/About Us/About Us.jsx';
 
 function App() {
     return(
@@ -17,10 +18,11 @@ function App() {
                     ["Services", "services"],
                     ["Common Electrical Problem", "FAQ"]
                 ]} />}>
-                   
+
+                    <Route path="/" element={<Navigate to="/home" />} /> 
                     <Route path="/products" element={<ProductsPage />}/>
-                    <Route index path="/home" element={<LandingPage />} />
-                    <Route path="/about" element={<h1>About Us</h1>} />
+                    <Route  path="/home" element={<LandingPage />} />
+                    <Route path="/about" element={<AboutPage />} />
                     <Route path="/services" element={<ServicePage />} />
                     <Route path="/faq" element={<FAQPage />} />
                 </Route>
